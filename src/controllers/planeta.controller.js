967ef17644ee314ce7585aa3440ed910f599fcb0
JuanResk <juanjo_resk@live.com>
2025-12-17 16:20:40 -0300

    Modificación a planeta routes y controller para la vista de API de un solo planeta

--- src/controllers/planeta.controller.js
+++ src/controllers/planeta.controller.js
@@ -22,3 +22,39 @@ exports.getAllplaneta = async (req, res) => {
     res.status(500).json({ error: 'Error al obtener los datos' });
   }
 };
+
+exports.getplanetaByidPlanet = async (req, res) => {
+  const id = Number(req.params.id);
+
+  if (!Number.isInteger(id)) {
+    return res.status(400).json({ error: 'ID inválido' });
+  }
+
+  try {
+    const [planetaRows] = await sistemaplanetas.query(
+      'SELECT * FROM planeta WHERE idPlanet = ?',
+      [id]
+    );
+
+    if (planetaRows.length === 0) {
+      return res.status(404).json({ error: 'No encontrado' });
+    }
+
+    const [lunaRows] = await sistemaplanetas.query(
+      'SELECT * FROM luna WHERE idPlanet = ?',
+      [id]
+    );
+
+    res.json({
+      ...planetaRows[0],
+      luna: lunaRows
+    });
+
+  } catch (error) {
+    console.error('ERROR REAL:', error);
+    res.status(500).json({
+      error: 'Error al obtener el registro',
+      detalle: error.message
+    });
+  }
+};
--- src/routes/planeta.routes.js
+++ src/routes/planeta.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const planetaController = require('../controllers/planeta.controller');
 
 router.get('/', planetaController.getAllplaneta);
+router.get('/:id', planetaController.getplanetaByidPlanet);
 
 module.exports = router;
