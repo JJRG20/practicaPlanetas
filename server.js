39cb8650df6d34e1df066033cf7f24428665d217
JuanResk <juanjo_resk@live.com>
2025-12-22 10:21:49 -0300

    Prueba de conexión a mysql con sequelize desde server.js

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+const sequelize = require('./src/config/database');
+
+sequelize.authenticate()
+  .then(() => console.log('Conectado a MySQL con Sequelize'))
+  .catch(err => console.error('Error de conexión:', err));
