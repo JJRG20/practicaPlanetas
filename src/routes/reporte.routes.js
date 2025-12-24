7ffce82f1c67868db6bc9d5967c3e3e85546fadc
JuanResk <juanjo_resk@live.com>
2025-12-24 15:49:00 -0300

    Correción de errores: .env se había borrado, y reporte.routes.js no hacía constancia de auth

--- src/routes/reporte.routes.js
+++ src/routes/reporte.routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const reporteController = require('../controllers/reporte.controller');
 
 const { allowRoles } = require('../middlewares/authorize');
+const { authenticate } = require('../middlewares/auth');
 
 // router.get('/idPlanet', reporteController.reporteMinluna);
 // router.get('/idPlanet', reporteController.reporteCountlunas);
-router.get('/idPlanet', allowRoles('admin', 'astro'), reporteController.reporteCount);
+router.get('/idPlanet', authenticate,  allowRoles('admin', 'astro'), reporteController.reporteCount);
 
 module.exports = router;
