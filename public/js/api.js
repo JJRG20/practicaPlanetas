a54690dcdf74d4838e1482f72c90a174574a4a89
JuanResk <juanjo_resk@live.com>
2025-12-26 11:37:00 -0300

    Creación de api.js para acceder a la API desde la UI

---
+++ public/js/api.js
@@ -0,0 +1,23 @@
+export async function apiFetch(url, options = {}) {
+  const token = localStorage.getItem('token');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    ...(options.headers || {})
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const res = await fetch(url, {
+    ...options,
+    headers
+  });
+
+  if (!res.ok) {
+    throw new Error('Error en la petición');
+  }
+
+  return res.json();
+}
